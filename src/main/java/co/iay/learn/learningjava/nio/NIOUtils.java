713bd3b9baa599e10d1bebf6a72c10afce043389
liaoaoyang <liaoaoyang@gmail.com>
2018-07-27 08:36:19 +0800

    Add constants

---
+++ src/main/java/co/iay/learn/learningjava/nio/NIOUtils.java
@@ -0,0 +1,20 @@
+package co.iay.learn.learningjava.nio;
+
+public class NIOUtils {
+    final public static int    REQ                        = 1;
+    final public static long   DEFAULT_TIMEOUT_MS         = 5000L;
+    final public static int    DEFAULT_BUFFER_SIZE        = 128;
+    final public static int    DEFAULT_THREAD_POOL_SIZE   = 128;
+    final public static String WROTE_BYTES                = "WROTE_BYTES";
+    final public static String WRITE_BUFFER               = "WRITE_BUFFER";
+    final public static String READ_BUFFER                = "READ_BUFFER";
+    final public static String CHANNEL                    = "CHANNEL";
+    final public static String CHANNEL_ID                 = "CHANNEL_ID";
+    final public static String STRING_BUILDER             = "STRING_BUILDER";
+    final public static String REQUEST_START_TIME_MS      = "REQUEST_START_TIME_MS";
+    final public static String CONTENT_LENGTH             = "CONTENT_LENGTH";
+    final public static String HTTP_HEADER_CONTENT_LENGTH = "CONTENT-LENGTH";
+    final public static String HTTP_BODY_START_INDEX      = "HTTP_BODY_START_INDEX";
+    final public static String CRLF                       = "\r\n";
+    final public static String HTTP_HEADER_END            = "\r\n\r\n";
+}
