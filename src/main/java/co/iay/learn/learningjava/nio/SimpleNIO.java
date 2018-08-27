16636b2594db61cbd187610399d12f2b5b5c43f5
liaoaoyang <liaoaoyang@gmail.com>
2018-08-28 00:02:17 +0800

    Add constants

---
+++ src/main/java/co/iay/learn/learningjava/nio/SimpleNIO.java
@@ -0,0 +1,9 @@
+package co.iay.learn.learningjava.nio;
+
+
+public class SimpleNIO {
+    final public static int    BUFFER_SIZE           = 1024;
+    final public static String BUFFER                = "BUFFER";
+    final public static String SOCKET_CHANNEL        = "SOCKET_CHANNEL";
+    final public static String SERVER_SOCKET_CHANNEL = "SERVER_SOCKET_CHANNEL";
+}
