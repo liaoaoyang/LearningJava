23cffba7884b95acf894cfcd4502b9c4fc08ab52
liaoaoyang <liaoaoyang@gmail.com>
2018-09-09 01:09:39 +0800

    Make long connection

--- scripts/simple_nio_test_echo_client.go
+++ scripts/simple_nio_test_echo_client.go
@@ -16,18 +16,18 @@ import (
 )
 
 func getRandomString(strLen int) []byte {
-    bytes := []byte("01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    charTable := []byte("01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
     var result []byte
     r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
     for i := 0; i < strLen; i++ {
-        result = append(result, bytes[r.Intn(len(bytes))])
+        result = append(result, charTable[r.Intn(len(charTable))])
     }
 
     return result
 }
 
-func handle(concurrentChan chan int, resultChan chan int, requestId int, host string, port string, strLen *int) {
+func handle(concurrentChan chan int, resultChan chan int, requestId int, host string, port string, strLen *int, repeat int, intervalS int) {
     conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", host, port))
     if err != nil {
         _ = <-concurrentChan
@@ -37,67 +37,82 @@ func handle(concurrentChan chan int, resultChan chan int, requestId int, host st
     }
 
     resp := bufio.NewReader(conn)
-    data2send := getRandomString(*strLen)
 
-    wrote, err := conn.Write(data2send)
-
-    if err != nil {
-        fmt.Printf("Request #%d conn.Write(): %s and wrote %s\n", requestId, err.Error(), string(wrote))
-        _ = <-concurrentChan
-        resultChan <- -2
-        conn.Close()
-        return
-    }
-
-    nowReadLen := 0
-    var data2receive []byte;
-    for ; nowReadLen < *strLen; {
-        nowRead, err := resp.ReadByte()
+    for r := 0; r < repeat; r++ {
+        data2send := getRandomString(*strLen)
+        wrote, err := conn.Write(data2send)
 
         if err != nil {
-            fmt.Printf("Request #%d resp.ReadByte(): %s\n", requestId, err.Error())
+            fmt.Printf("Request #%d conn.Write(): %s and wrote %s\n", requestId, err.Error(), string(wrote))
             _ = <-concurrentChan
-            resultChan <- -3
+            resultChan <- -2
             conn.Close()
             return
         }
 
-        nowReadLen += len(string(nowRead))
-        data2receive = append(data2receive, nowRead)
+        nowReadLen := 0
+        var data2receive []byte
+        for ; nowReadLen < *strLen; {
+            nowRead, err := resp.ReadByte()
 
-        if nowReadLen >= *strLen {
-            conn.Close()
-            if bytes.Compare(data2send, data2receive) != 0 {
-                fmt.Printf("Request #%d Read/Write data mismatch\n", requestId)
+            if err != nil {
+                fmt.Printf("Request #%d resp.ReadByte(): %s\n", requestId, err.Error())
                 _ = <-concurrentChan
-                resultChan <- -4
+                resultChan <- -3
+                conn.Close()
                 return
             }
-            _ = <-concurrentChan
-            resultChan <- 0
-            return
+
+            nowReadLen += len(string(nowRead))
+            data2receive = append(data2receive, nowRead)
+
+            if nowReadLen >= *strLen {
+                if repeat > 1 {
+                    break
+                }
+
+                conn.Close()
+                if bytes.Compare(data2send, data2receive) != 0 {
+                    fmt.Printf("Request #%d Read/Write data mismatch\n", requestId)
+                    _ = <-concurrentChan
+                    resultChan <- -4
+                    return
+                }
+                _ = <-concurrentChan
+                resultChan <- 0
+                return
+            }
+        }
+
+        if r < repeat {
+            time.Sleep(time.Duration(intervalS) * time.Second)
         }
     }
+
+    conn.Close()
+    _ = <-concurrentChan
+    resultChan <- 0
 }
 
-func doRequest(concurrentChan chan int, resultChan chan int, requestId int, host string, port string, strLen *int) {
-    go handle(concurrentChan, resultChan, requestId, host, port, strLen)
+func doRequest(concurrentChan chan int, resultChan chan int, requestId int, host string, port string, strLen *int, repeat int, intervalS int) {
+    handle(concurrentChan, resultChan, requestId, host, port, strLen, repeat, intervalS)
 }
 
 func main() {
-    var needExit bool
+    needExit := 0
 
-    needExit = false
     host := flag.String("h", "127.0.0.1", "Host")
     port := flag.String("p", "12345", "Port")
     portsStr := flag.String("P", "", "Ports separated by comma")
     strLen := flag.Int("l", 32, "Random string length")
     concurrent := flag.Int("c", 10, "Concurrent")
     requestNum := flag.Int("n", 10, "Request number")
+    intervalS := flag.Int("i", 0, "Send data after (i) second(s)")
+    repeat := flag.Int("r", 1, "Repeatedly sending data for (r) times")
     flag.Parse()
     ports := strings.Split(*portsStr, ",")
 
-    fmt.Printf("host=%s port=%s strLen=%d concurrent=%d requestNum=%d portsStr=%s\n", *host, *port, *strLen, *concurrent, *requestNum, *portsStr)
+    fmt.Printf("host=%s port=%s strLen=%d concurrent=%d requestNum=%d portsStr=%s intervalS=%d repeat=%d\n", *host, *port, *strLen, *concurrent, *requestNum, *portsStr, *intervalS, *repeat)
 
     handled := 0
     handleSuccess := 0
@@ -117,17 +132,25 @@ func main() {
     signal.Notify(sigs, syscall.SIGINT)
 
     go func() {
-        sig := <-sigs
+        for ; needExit <= 1; {
+            sig := <-sigs
 
-        if sig == syscall.SIGINT {
-            fmt.Printf("\nSent %d requests and now need exit after finished request\n", alreadySent)
-            needExit = true
+            if sig == syscall.SIGINT {
+                needExit++
+
+                if needExit > 1 {
+                    fmt.Printf("\nForce exit now!\n")
+                    os.Exit(0)
+                }
+
+                fmt.Printf("\nSent %d requests and now need exit after finished request\n", alreadySent)
+            }
         }
     }()
 
     go func() {
         for i := 1; i <= *requestNum; i++ {
-            if needExit {
+            if needExit > 0 {
                 break
             }
 
@@ -139,13 +162,13 @@ func main() {
             }
 
             concurrentChan <- i
-            go doRequest(concurrentChan, resultChan, i, *host, tempPort, strLen)
+            go doRequest(concurrentChan, resultChan, i, *host, tempPort, strLen, *repeat, *intervalS)
             alreadySent++
         }
     }()
 
     for ; handled < *requestNum; {
-        if needExit && handled >= alreadySent {
+        if needExit > 0 && handled >= alreadySent {
             break
         }
 
@@ -160,7 +183,7 @@ func main() {
 
         tenPercentNum := *requestNum / 10
 
-        if (handled) > 0 && (handled)%tenPercentNum == 0 {
+        if (handled) > 0 && tenPercentNum > 0 && (handled)%tenPercentNum == 0 {
             fmt.Printf("Requested finished %d(success=%d fail=%d) with %d go routine\n", handled, handleSuccess, handleFail, runtime.NumGoroutine())
         }
     }
