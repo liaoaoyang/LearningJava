4dc43efff7309c5f9d02b51fd6d04428cbd2d0cc
liaoaoyang <liaoaoyang@gmail.com>
2018-10-07 19:53:44 +0800

    add un-version code

---
+++ scripts/simple_nio_test_utils.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "math/rand"
+    "time"
+)
+
+func getRandomString(strLen int) []byte {
+    charTable := []byte("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    var result []byte
+    r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+    for i := 0; i < strLen; i++ {
+        result = append(result, charTable[r.Intn(len(charTable))])
+    }
+
+    return result
+}
